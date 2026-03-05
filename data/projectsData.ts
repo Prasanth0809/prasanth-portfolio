b2860fc8fc384ee039e6ff0df041fbb75724a723
Prasanth0809 <101864960+Prasanth0809@users.noreply.github.com>
2026-03-05 17:28:25 -0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,10 @@
 const projectsData = [
   {
-    title: 'Azure Secure Storage Architecture',
-    description:
-      'Designed a secure Azure Storage architecture using private endpoints, RBAC, and encryption to protect cloud data.',
-    imgSrc: '/static/images/azure-storage.png',
-    href: '/blog/azure-secure-storage-architecture',
-  },
+  title: 'Azure Secure Storage Architecture',
+  description: 'Designed a secure Azure Storage architecture using private endpoints, RBAC, and encryption to protect cloud data.',
+  imgSrc: '/static/images/azure-secure-storage-architecture.png',
+  href: '/blog/azure-secure-storage-architecture',
+}
 ]
 
 export default projectsData
