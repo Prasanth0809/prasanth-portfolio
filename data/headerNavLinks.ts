327cec114b9e67c89dc01da85a11c897a854a3ac
Prasanth0809 <101864960+Prasanth0809@users.noreply.github.com>
2026-03-07 17:42:38 -0500

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Case Studies' },
   { href: '/projects', title: 'Projects' },
-  { href: '/static/resume.pdf', title: 'Resume' },
+  { href: '/static/resume.pdf', title: 'Resume', target: '_blank' },
   { href: '/about', title: 'About' },
 ]
 
